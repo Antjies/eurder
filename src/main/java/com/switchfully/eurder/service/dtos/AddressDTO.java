72c6a3ac5884965e6557438eb239debeb87c72ee
Joris Ceuppens <jorisc50@gmail.com>
2023-03-28 10:42:23 +0200

    still same

---
+++ src/main/java/com/switchfully/eurder/service/dtos/AddressDTO.java
@@ -0,0 +1,45 @@
+package com.switchfully.eurder.service.dtos;
+
+public class AddressDTO{
+
+    private String streetName;
+    private String number;
+    private String postalCode;
+    private String city;
+
+    public AddressDTO setStreetName(String streetName) {
+        this.streetName = streetName;
+        return this;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public AddressDTO setNumber(String number) {
+        this.number = number;
+        return this;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public AddressDTO setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+        return this;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public AddressDTO setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
--- src/main/java/com/switchfully/eurder/service/dtos/CustomerDTO.java
+++ src/main/java/com/switchfully/eurder/service/dtos/CustomerDTO.java
@@ -1,4 +1,66 @@
 package com.switchfully.eurder.service.dtos;
 
-public class CustomerDTO {
+import com.switchfully.eurder.domain.models.Address;
+
+public class CustomerDTO{
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private AddressDTO addressDTO;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public CustomerDTO setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public CustomerDTO setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public CustomerDTO setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public CustomerDTO setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    public AddressDTO getAddressDTO() {
+        return addressDTO;
+    }
+
+    public CustomerDTO setAddressDTO(AddressDTO addressDTO) {
+        this.addressDTO = addressDTO;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public CustomerDTO setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
 }
