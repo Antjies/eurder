4bbb5b5c38906942c624f77ac3ef9de147945e62
Joris Ceuppens <jorisc50@gmail.com>
2023-03-28 12:50:21 +0200

    logging added

--- src/main/java/com/switchfully/eurder/api/CustomerController.java
+++ src/main/java/com/switchfully/eurder/api/CustomerController.java
@@ -2,6 +2,8 @@ package com.switchfully.eurder.api;
 
 import com.switchfully.eurder.service.CustomerService;
 import com.switchfully.eurder.service.dtos.CreateCustomerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final Logger myLogger = LoggerFactory.getLogger(CustomerController.class);
 
     @Autowired
     public CustomerController(CustomerService customerService) {
@@ -20,6 +23,7 @@ public class CustomerController {
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerNewCustomer(@RequestBody CreateCustomerDTO createCustomerDTO) {
+        myLogger.info("Handler method registerNewCustomer is called");
         customerService.registerNewCustomer(createCustomerDTO);
     }
 
